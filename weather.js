afaf031b6aa8ad6043eafa9b727f9383f0642ac1
ReaganG <rbgrunwald1997@gmail.com>
2021-01-13 14:35:32 -0600

    stored vars from response that are needed

--- weather.js
+++ weather.js
@@ -1,10 +1,11 @@
 var weatherURL = "https://api.weatherbit.io/v2.0/forecast/daily?" //city="dallas";
 var weatherKey = "fb5c4c3b9af04b90ba63cec252a9d051";
 
+
 $(document).ready(function(){
-
+   
     getWeather();
-
+    console.log(icon);
 });
 
 function getWeather(){
@@ -14,5 +15,10 @@ function getWeather(){
         url: weatherURL + "city=dallas&key=" + weatherKey
     }).then(function(res){
         console.log(res);
+        var high, description, icon;
+        
+        high = Math.round((res.data[0].max_temp * (9/5)) + 32);
+        description = res.data[0].weather.description;
+        icon = res.data[0].weather.icon;
     })
 }
