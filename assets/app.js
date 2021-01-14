2c574a6604f43e661cc889f53460091ffc8ce2bf
ReaganG <rbgrunwald1997@gmail.com>
2021-01-14 12:51:31 -0600

    added find local brews button if it is a good day

--- assets/app.js
+++ assets/app.js
@@ -9,7 +9,6 @@ var weatherURL = "https://api.weatherbit.io/v2.0/forecast/daily?" //city="dallas
 var weatherKey = "fb5c4c3b9af04b90ba63cec252a9d051";
 
 var goodWeather = [800, 801, 802, 803];
-var goodDay = false;
 
 var yesNo;
 
@@ -32,6 +31,8 @@ function getWeather() {
 
         $(".forecast-container").empty();
         for (var i = 0; i < 7; i++) {
+            var goodDay = false;
+
             code = res.data[i].weather.code;
             high = Math.round((res.data[i].max_temp * (9 / 5)) + 32);
             description = res.data[i].weather.description;
@@ -45,22 +46,46 @@ function getWeather() {
                 yesNo = "Nahh";
             }
 
-            $(".forecast-container").append(/*html*/ `
-            <div class="day-card">
+            if(goodDay){
+                $(".forecast-container").append(/*html*/ `
+                <div class="day-card">
+        
+                    <p><span class="date">${res.data[i].valid_date}</span></p>
+                    <p>Good Day For a Beer?</p>
     
-                <p><span class="date">${res.data[i].valid_date}</span></p>
-                <p>Good Day For a Beer?</p>
-
-                <p><span class="yes-no">${yesNo}</span></p>
-
-                <img src="https://www.weatherbit.io/static/img/icons/${icon}.png">
-
-                <p><span class="temp">${high}</span>&degF</p>
-
-                <p>Conditions: <span class="conditions">${description}</span></p>
-
-            </div>
-            `)
+                    <p><span class="yes-no">${yesNo}</span></p>
+    
+                    <img src="https://www.weatherbit.io/static/img/icons/${icon}.png">
+    
+                    <p><span class="temp">${high}</span>&degF</p>
+    
+                    <p>Conditions: <span class="conditions">${description}</span></p>
+    
+                    <button class="local-brew">Find Me Local Breweries!</button>
+    
+                </div>
+                `)
+            }
+            else{
+                $(".forecast-container").append(/*html*/ `
+                <div class="day-card">
+        
+                    <p><span class="date">${res.data[i].valid_date}</span></p>
+                    <p>Good Day For a Beer?</p>
+    
+                    <p><span class="yes-no">${yesNo}</span></p>
+    
+                    <img src="https://www.weatherbit.io/static/img/icons/${icon}.png">
+    
+                    <p><span class="temp">${high}</span>&degF</p>
+    
+                    <p>Conditions: <span class="conditions">${description}</span></p>
+    
+    
+                </div>
+                `)
+            }
+           
         }
 
     })
